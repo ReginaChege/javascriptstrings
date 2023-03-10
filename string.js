9e4e8615bda6794a185dc1aa440215d52522eb47
ReginaChege <reginachegewairimu@gmail.com>
2023-03-10 19:49:41 +0300

    first commit

---
+++ string.js
@@ -0,0 +1,50 @@
+// Extract the first four characters from the string below;
+// "beautiful"
+let girl="beautiful";
+console.log(girl[0]);
+console.log(girl[1]);
+console.log(girl[2]);
+console.log(girl[3]);
+// Insert the following string at the tenth index of the below variable:
+// "eat"
+const food = "I did not have appetite today"
+let food2 = food.slice(0,3)+" eat"+food.slice(4,5);
+console.log(food2)
+// Convert the following strings into the specified format:
+// 1. UpperCase: "CONfidant"
+// 2. LowerCase: "amazing", "beautiFUL"
+// 3. Title case "A busy office"
+let word = "CONFidant"
+let word2 =word.toUpperCase()
+console.log(word2)
+
+let sentence ="amazing"
+let sentence2 =sentence.toLowerCase
+console.log(sentence2)
+
+// using JavaScript, find the following words from the following strings:
+// 1. "market"
+// const string1 = "The lady went to the market with her sister";
+// 2. "season"
+// const string2 = "My favorite season is spring"
+const string1 ="The lady went to the market with her sister"
+const market= string1.indexOf("market")
+console.log(market)
+
+const string2="My favourite season is spring"
+const season=string2.indexOf("season")
+console.log(season)
+
+// Count how many times the following string appears in the string variable:
+// 1. "the"
+// 2."s"
+// const story= "She sells sea shells at the sea shore
+const sen = "Today is the day that we start learning  javascript"
+let sen2 = food.slice(0,1)+food.slice(2,7);
+console.log(sen2)
+
+const info= "Smart thinking is key"
+let info2 = food.slice(1,3)
+console.log(info2)
+
+
